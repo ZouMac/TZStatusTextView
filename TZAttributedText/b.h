030e4f176a64522f9a91835eac3d545f09e54424
tanzou <menghuan.lq@qq.com>
2017-11-21 09:36:08 +0800

    B

---
+++ TZAttributedText/b.h
@@ -0,0 +1,13 @@
+//
+//  b.h
+//  TZAttributedText
+//
+//  Created by tanzou on 2017/11/21.
+//  Copyright © 2017年 Tanz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface b : UIButton
+
+@end
---
+++ TZAttributedText/b.m
@@ -0,0 +1,21 @@
+//
+//  b.m
+//  TZAttributedText
+//
+//  Created by tanzou on 2017/11/21.
+//  Copyright © 2017年 Tanz. All rights reserved.
+//
+
+#import "b.h"
+
+@implementation b
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
